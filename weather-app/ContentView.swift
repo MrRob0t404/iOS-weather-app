bab3a39b7dc74e7b8ffd4d76872383d9dbccaee4
Simon Gaviria <simongaviria1@gmail.com>
2022-01-11 16:50:57 -0500

    Initial Commit

---
+++ weather-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather-app
+//
+//  Created by Simon Gaviria on 1/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
